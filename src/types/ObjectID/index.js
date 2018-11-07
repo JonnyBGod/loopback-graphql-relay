2cdf65e204a380310c6ca0f268f30e9463db9ade
João Ribeiro <jonnybgod@gmail.com>
2018-11-07 23:55:49 +0000

    fix(core): support array for objectIds

--- src/types/ObjectID/index.js
+++ src/types/ObjectID/index.js
@@ -7,7 +7,16 @@ module.exports = new GraphQLScalarType({
   description:
     'The `ObjectID` scalar type represents a [`BSON`](https://en.wikipedia.org/wiki/BSON) ID commonly used in `mongodb`.',
   serialize(_id) {
-    if (typeof _id === 'object' && typeof _id.toHexString !== 'undefined') {
+    if (Array.isArray(_id)) {
+      return _id.map((item) => {
+        if (typeof item === 'object' && typeof item.toHexString !== 'undefined') {
+          return item.toHexString();
+        } else if (typeof item === 'string') {
+          return item;
+        }
+        throw new Error(`${Object.getPrototypeOf(item).constructor.name} not convertible to `);
+      });
+    } else if (typeof _id === 'object' && typeof _id.toHexString !== 'undefined') {
       return _id.toHexString();
     } else if (typeof _id === 'string') {
       return _id;
@@ -15,7 +24,14 @@ module.exports = new GraphQLScalarType({
     throw new Error(`${Object.getPrototypeOf(_id).constructor.name} not convertible to `);
   },
   parseValue(_id) {
-    if (typeof _id === 'string') {
+    if (Array.isArray(_id)) {
+      return _id.map((item) => {
+        if (typeof item === 'string') {
+          return ObjectID.createFromHexString(item);
+        }
+        throw new Error(`${typeof item} not convertible to ObjectID`);
+      });
+    } else if (typeof _id === 'string') {
       return ObjectID.createFromHexString(_id);
     }
     throw new Error(`${typeof _id} not convertible to ObjectID`);
